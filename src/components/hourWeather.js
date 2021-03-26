1341b40e415f38ffa737358a261d68c120f86e57
anuvarghese1395 <anuvarghese1395@gmail.com>
2021-03-26 12:35:28 +0100

    addes 12 hour time from present

--- src/components/hourWeather.js
+++ src/components/hourWeather.js
@@ -1,48 +1,57 @@
-import GridList from "@material-ui/core/GridList";
-import GridTile from "@material-ui/core/GridListTile";
-import Paper from "@material-ui/core/Paper";
-import { makeStyles } from "@material-ui/core/styles";
+import GridList from '@material-ui/core/GridList';
+import GridTile from '@material-ui/core/GridListTile';
+import { makeStyles } from '@material-ui/core/styles';
+import BeachAccessRoundedIcon from '@material-ui/icons/BeachAccessRounded';
+import WbSunnycon from '@material-ui/icons/WbSunny';
 
 const useStyles = makeStyles((theme) => ({
   root: {
-    display: "list-item",
-    flexWrap: "wrap",
-    justifyContent: "space-around",
-    overflow: "auto",
+    display: 'list-item',
+    flexWrap: 'wrap',
+    justifyContent: 'space-around',
+    overflow: 'auto',
     backgroundColor: theme.palette.background.paper,
-    borderRadius: "25px",
+    borderRadius: '25px',
   },
   gridList: {
-    flexWrap: "nowrap",
-    // Promote the list into his own layer on Chrome. This cost memory but helps keeping high FPS.
-    transform: "translateZ(0)",
+    flexWrap: 'nowrap',
+    transform: 'translateZ(0)',
   },
   title: {
-    color: theme.palette.primary.light,
+    color: theme.palette.primary,
   },
 }));
 
 function HourlyWeatherData({ weather }) {
   const classes = useStyles();
-  console.log("hr", weather.hour);
-  const weatherData = weather.hour;
-  var d = new Date();
-  var hour = d.getHours();
 
+  const weatherData = weather.hour;
+  const d = new Date();
+  let hour = d.getHours();
+  const currentWeather = weatherData.slice(hour, hour + 12);
+  console.log('hours', currentWeather);
   return (
     <div className={classes.root}>
       <GridList className={classes.gridList} cols={6} spacing={3}>
-        {
-          weatherData.filter(
-            (weather) => weather.time.split(" ").splice(1, 1) === hour
-          )
-          
-          //   .map((filteredTime) => (
-          //     <GridTile key={weather.id} className={classes.title}>
-          //       <h1>{filteredTime}</h1>
-          //     </GridTile>
-          //   ))}
-        }
+        {currentWeather.map((weather) => {
+          return (
+            <GridTile key={weather.id} className={classes.title}>
+              <h1>{weather.time.split(' ')[1]}</h1>
+
+              {weather.chance_of_rain === '0' ? (
+                <h1>{<WbSunnycon />}</h1>
+              ) : (
+                <h1>
+                  <BeachAccessRoundedIcon />
+                </h1>
+              )}
+              <h1>
+                {weather.temp_c}
+                <span>&deg;C</span>
+              </h1>
+            </GridTile>
+          );
+        })}
       </GridList>
     </div>
   );
