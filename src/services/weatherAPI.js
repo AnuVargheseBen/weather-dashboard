502aa3622d3c2a824aaf4106bed8aba86db56469
anuvarghese1395 <anuvarghese1395@gmail.com>
2021-04-05 23:36:25 +0200

    updates weather ai to https

--- src/services/weatherAPI.js
+++ src/services/weatherAPI.js
@@ -4,7 +4,7 @@ async function getWeatherData(locationName) {
 
   const options = {
     method: "GET",
-    url: `http://api.weatherapi.com/v1/forecast.json?key=6124b5ab6dd44fdcb3b84806211203&q=${locationName}&days=5&aqi=no&alerts=no`,
+    url: `https://api.weatherapi.com/v1/forecast.json?key=6124b5ab6dd44fdcb3b84806211203&q=${locationName}&days=5&aqi=no&alerts=no`,
     
   };
 
